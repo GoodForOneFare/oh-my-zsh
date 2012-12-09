bd00ffcaf3006d9874242227b60209e77d017c0a
Gord Pearson <Gord@Gords-MacBook-Pro.local>
2012-12-09 14:54:39 -0500

    Toushay aliases.

---
+++ plugins/toushay/toushay.plugin.zsh
@@ -0,0 +1,4 @@
+alias p="cd ~/Documents/workspace/Toushay-Portal"
+alias d="cd ~/Documents/workspace/Toushay-Dev/server"
+alias cu='p; cd tests; bundle exec cucumber && terminal-notifier -message "finished cucumber"'
+
